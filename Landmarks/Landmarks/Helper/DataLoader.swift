32679374958bf2ed6190c5ccc8c729cc0ca50b19
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-12 20:36:16 +0500

    Refactor DataLoader to handle error cases instead of throwing

--- Landmarks/Landmarks/Helper/DataLoader.swift
+++ Landmarks/Landmarks/Helper/DataLoader.swift
@@ -8,23 +8,29 @@
 import Foundation
 
 var landmarks: [LandmarkViewModel] = {
-    (try? DataLoader<[Landmark]>.load(from: "landmarkData.json").map{LandmarkViewModel($0)}) ?? []
+    DataLoader<[Landmark]>.load(from: "landmarkData.json").map{ LandmarkViewModel($0) }
 }()
 
 class DataLoader<T: Decodable> {
-    static func load(from file: String) throws -> T {
+    static func load(from file: String) -> T {
+        var data: Data
+        
         guard let url = Bundle.main.url(forResource: file, withExtension: nil) else {
-            throw NSError(domain: "No file found with name \(file)", code: 0)
+            fatalError("No file found with name \(file)")
         }
         
-        guard let data = try? Data(contentsOf: url) else {
-            throw NSError(domain: "Data cannot be read from \(file) file", code: 0)
+        do {
+            data = try Data(contentsOf: url)
+        }
+        catch {
+            fatalError("Data cannot be read from \(file) file")
         }
         
-        guard let model = try? JSONDecoder().decode(T.self, from: data) else {
-            throw NSError(domain: "Data present in \(file) file cannot be decoded into \(type(of: T.self)) type", code: 0)
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
         }
-        
-        return model
+        catch {
+            fatalError("Data present in \(file) file cannot be decoded into \(type(of: T.self)) type")
+        }
     }
 }
