a83a0d40ad8d436223e77f61eda565b35b549e8d
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-12 14:51:55 +0500

    Create Project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Fardan Akhter on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
