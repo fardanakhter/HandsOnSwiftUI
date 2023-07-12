c28f2f60b4ec71b785ae82567226b4c6ac67de4c
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-12 19:07:00 +0500

    Add Models for landmark

---
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -0,0 +1,26 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Fardan Akhter on 12/07/2023.
+//
+
+import Foundation
+import CoreLocation
+
+struct Landmark: Codable {
+    let id: String
+    let name: String
+    let category: String
+    let city: String
+    let state: String
+    let park: String
+    let description: String
+    let imageName: String
+    let coordinates: Coordinates
+}
+
+struct Coordinates: Codable {
+    let longitude: Double
+    let latitude: Double
+}
---
+++ Landmarks/Landmarks/Models/LandmarkViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarkViewModel.swift
+//  Landmarks
+//
+//  Created by Fardan Akhter on 12/07/2023.
+//
+
+import Foundation
+import CoreLocation
+
+struct LandmarkViewModel {
+    let landmark: Landmark
+    
+    init(_ landmark: Landmark) {
+        self.landmark = landmark
+    }
+    
+    var landmarkImage: String {
+        landmark.imageName
+    }
+    
+    var landmarkName: String {
+        landmark.name
+    }
+    
+    var landmarkPark: String {
+        landmark.park
+    }
+    
+    var landmarkState: String {
+        landmark.state
+    }
+    
+    var landmarkDescription: String {
+        landmark.description
+    }
+    
+    var coordinates: CLLocationCoordinate2D {
+        .init(latitude: landmark.coordinates.latitude, longitude: landmark.coordinates.longitude)
+    }
+}
