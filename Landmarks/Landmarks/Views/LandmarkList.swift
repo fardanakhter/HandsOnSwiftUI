a1b8ee23a6f1b9da35e32ad50002e828ce8e6759
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-12 20:48:22 +0500

    Add 'LandmarkList' view to display list or 'LandmarkRow' views

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Fardan Akhter on 12/07/2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+            LandmarkRow(landmark: landmarks[3])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
